4c70ee49a789d3d3f68695406bedb4cb5ebd218a
janggalman <janggalman@gmail.com>
2020-06-13 11:52:51 +0900

    서버에 로그인 실제 요청 기능 추가 =>아직 사용 X

--- app/src/main/java/kr/tjoeun/apipractice_20200613/LoginActivity.kt
+++ app/src/main/java/kr/tjoeun/apipractice_20200613/LoginActivity.kt
@@ -2,6 +2,7 @@ package kr.tjoeun.apipractice_20200613
 
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_login.*
+import kr.tjoeun.apipractice_20200613.utils.ServerUtil
 
 class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +19,9 @@ class LoginActivity : BaseActivity() {
             val iinputPw = pwEdt.text.toString()
 
 //          실제로 서버에 두개의 변수를 전달해서 로그인 시도
+//           별개의 클래스 (ServerUtil)에 서버 요청 기능을 만들고, 화면에서는 이를 사용.
+
+
 
 
         }
