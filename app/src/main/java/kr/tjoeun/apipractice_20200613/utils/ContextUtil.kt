0288ee663f1ea143709183b3bce455c5eec310e2
janggalman <janggalman@gmail.com>
2020-06-14 11:04:56 +0900

    자동로그인 여부 저장 / 조회 기능 추가

--- app/src/main/java/kr/tjoeun/apipractice_20200613/utils/ContextUtil.kt
+++ app/src/main/java/kr/tjoeun/apipractice_20200613/utils/ContextUtil.kt
@@ -6,10 +6,22 @@ class ContextUtil {
 
     companion object {
 //        메모장의 파일이름에 대응되는 개념으로 만든 변수
-        val prefName = "APIPracticePreference"
+        private val prefName = "APIPracticePreference"
 
 //        저장된 항목이름 변수 설정
-        val USER_TOKEN = "USER_TOKEN"
+        private val USER_TOKEN = "USER_TOKEN"
+        private val AUTO_LOGIN = "AUTO_LOGIN"
+
+//        자동로그인 getter / setter
+        fun setAutoLogin (context: Context, autoLogin:Boolean) {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.edit().putBoolean(AUTO_LOGIN , autoLogin).apply()
+        }
+
+        fun isAutoLogin(context: Context) : Boolean {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            return pref.getBoolean(AUTO_LOGIN, false)
+        }
 
 //        토큰 저장 기능
         fun setUserToken(context: Context, token: String) {
