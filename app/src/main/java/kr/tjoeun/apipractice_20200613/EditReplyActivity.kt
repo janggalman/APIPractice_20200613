0eb8ae57547f7809ad41e40afdd32fb1c100ff14
janggalman <janggalman@gmail.com>
2020-06-20 12:04:13 +0900

    한번 등록하면 내용을 수정할수 없다고안내

--- app/src/main/java/kr/tjoeun/apipractice_20200613/EditReplyActivity.kt
+++ app/src/main/java/kr/tjoeun/apipractice_20200613/EditReplyActivity.kt
@@ -1,7 +1,9 @@
 package kr.tjoeun.apipractice_20200613
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_edit_reply.*
 
 class EditReplyActivity : BaseActivity() {
@@ -14,6 +16,19 @@ class EditReplyActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        postReplyBtn.setOnClickListener {
+
+//            한번 등록하면 내용을 수정할 수 없다고 안내
+            val alert = AlertDialog.Builder(mContext)
+            alert.setTitle("내용 변경 불가 안내")
+            alert.setMessage("한번 등록한 의견은 내용을 수정할 수 없습니다. 의견을 등록한 뒤에는 재투표가 불가능합니다.")
+            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
+//                서버에 요청해서 의견 등록 처리 => 추가 데이터를 받어와야함.
+            })
+            alert.setNegativeButton("취소", null)
+            alert.show()
+        }
 
     }
 
