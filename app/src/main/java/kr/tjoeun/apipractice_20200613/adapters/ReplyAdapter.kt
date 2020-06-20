9b507a8c900b305e90b27db1f968f2dc549a318d
janggalman <janggalman@gmail.com>
2020-06-20 14:10:59 +0900

    좋아요 : 실제 서버 호출 + 갯수 변경사항도 같이 반영

--- app/src/main/java/kr/tjoeun/apipractice_20200613/adapters/ReplyAdapter.kt
+++ app/src/main/java/kr/tjoeun/apipractice_20200613/adapters/ReplyAdapter.kt
@@ -1,14 +1,19 @@
 package kr.tjoeun.apipractice_20200613.adapters
 
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import kr.tjoeun.apipractice_20200613.R
 import kr.tjoeun.apipractice_20200613.datas.TopicReply
+import kr.tjoeun.apipractice_20200613.utils.ServerUtil
+import org.json.JSONObject
 
 class ReplyAdapter(
     val mContext : Context,
@@ -45,6 +50,43 @@ class ReplyAdapter(
         replyBtn.text = "답글 : ${data.replyCount}"
         likeBtn.text = "좋아요 : ${data.likeCount}"
         dislikeBtn.text = "싫어요 : ${data.dislikeCount}"
+
+        //좋아요 / 싫어요 이벤트 처리
+        likeBtn.setOnClickListener {
+//좋아요 API호출 => 좋아요 누르기 /취소 처리
+            ServerUtil.postRequestReplyLike(mContext, data.id, true , object :ServerUtil.JsonResponseHandler {
+                override fun onResponse(json: JSONObject) {
+
+
+//            화면에 변경된 좋아요/싫어요 갯수 반영(응용)
+                val dataObj = json.getJSONObject("data")
+                    val reply = dataObj.getJSONObject("reply")
+
+//                    목록에서 꺼낸 data변수의 객체를 통채로 바꾸는건 불가능.
+//                    var 로 바꿔서 통째로 바꿔도 => 목록에는 반영되지 않음.
+
+//                    목록에서 꺼낸 data변수의 좋아요 갯수 /싫어요 갯수를 직접 변경
+                    data.likeCount = reply.getInt("like_count")
+                    data.dislikeCount = reply.getInt("dislike_count")
+
+//                    목록의 내용을 일부 변경 => 반영하려면
+//                    어댑터.notifyDataSEtChanged() 실행 필요함.
+//                    이미 어뎁터 내부에 있는 상황 = > 곧바로 notifyDataSetChanged() 실행 가능
+
+//                    runOnUiThread 로 처리 필요 => 어뎁터내부에선 사용 불가.
+//                    대체제 :Handler(Looper.getMainLooper()).post (UI쓰레드 접근하는 다른 방법)
+                    Handler(Looper.getMainLooper()).post{
+                        notifyDataSetChanged()
+                    }
+
+                }
+            })
+
+        }
+
+        dislikeBtn.setOnClickListener {
+
+        }
 
         return row
     }
