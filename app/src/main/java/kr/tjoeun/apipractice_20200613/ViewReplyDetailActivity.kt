33b49787e79b29158382f9fad0bf9919e9e50cfe
janggalman <janggalman@gmail.com>
2020-06-21 12:28:15 +0900

    답글 등록시 자동 새로고침 구현  => 기존에 달려있던 답글을 모두 날려버리고 새로 답글을 리스트에 띄우는 방식

--- app/src/main/java/kr/tjoeun/apipractice_20200613/ViewReplyDetailActivity.kt
+++ app/src/main/java/kr/tjoeun/apipractice_20200613/ViewReplyDetailActivity.kt
@@ -1,9 +1,11 @@
 package kr.tjoeun.apipractice_20200613
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_view_reply_detail.*
 import kotlinx.android.synthetic.main.activity_view_topic_detail.*
 import kr.tjoeun.apipractice_20200613.adapters.ReReplyAdapter
@@ -28,6 +30,50 @@ class ViewReplyDetailActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        postReReplyBtn.setOnClickListener {
+            val content = reReplyContentEdt.text.toString()
+
+//            답글 등록 API 찾아보기 활용법 숙지
+
+//            답글 등록 성공시 => 리스트뷰의 내용 새로고침
+
+//            서버에서 다시 답글 목록을 받아와서 추가
+
+
+//            한번 등록하면 내용을 수정할 수 없다고 안내
+            val alert = AlertDialog.Builder(mContext)
+            alert.setTitle("내용 변경 불가 안내")
+            alert.setMessage("한번 등록한 의견은 내용을 수정할 수 없습니다. 의견을 등록한 뒤에는 재투표가 불가능합니다.")
+            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
+//                서버에 요청해서 의견 등록 처리 => 추가 데이터를 받어와야함.
+                ServerUtil.postRequestTopicReReply(mContext,mReplyId , content, object :ServerUtil.JsonResponseHandler{
+                    override fun onResponse(json: JSONObject) {
+
+                        val code = json.getInt("code")
+
+                        if ( code == 200) {
+                            runOnUiThread {
+//                                서버에서 다시 의견에 대한 상세 현황 가져오기
+                                getReplyDetailFromServer()
+                            }
+
+                        }
+                        else {
+                            val message = json.getString("message")
+                            runOnUiThread {
+                                Toast.makeText(mContext, message , Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                    }
+
+                })
+            })
+            alert.setNegativeButton("취소", null)
+            alert.show()
+
+        }
 
     }
 
@@ -63,6 +109,9 @@ class ViewReplyDetailActivity : BaseActivity() {
 
 //                화면서 뿌려질 댓글 목록 담어주자.
                 var reReplies = reply.getJSONArray("replies")
+
+//                기존에 담겨있던 답글 목록을 날리고
+                reReplyList.clear()
 
                 for (i in 0..reReplies.length()-1) {
 //                    JSONArray 내부의 객체를  => TopicReply 로 변환 => reReplyList에 추가
